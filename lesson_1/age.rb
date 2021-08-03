adb492b64b4693b428f30bc952dcadec73ae02b1
visnu <visnu.santos1203@gmail.com>
2021-08-03 22:23:26 +0800

    first commit. Assignment done

---
+++ lesson_1/age.rb
@@ -0,0 +1,14 @@
+age = 25
+puts "How old are you?"
+puts "in 10 years  will be:"
+puts age + 10
+puts "in 20 years  will be:"
+puts age + 20
+puts "in 30 years  will be:"
+puts age + 30
+puts "in 40 years  will be:"
+puts age + 40
+
+
+
+
