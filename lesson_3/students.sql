1941e748254d7a516907cd0715073020064b3274
visnu <visnu.santos1203@gmail.com>
2021-08-12 18:50:27 +0800

    done with JOINS assignment

--- lesson_3/students.sql
+++ lesson_3/students.sql
@@ -39,6 +39,41 @@ SELECT count(first_name) FROM students;
 --Select all students with `location` is `Manila`
 SELECT * FROM students WHERE location = 'Manila';
 -- Display the average `age` of all `students`
-select AVG(age) from students;
+SELECT AVG(age) FROM students;
 --Display all `students` by `age` descending order
-select AVG(age) from students ORDER BY age DESC;
+SELECT * FROM students ORDER BY age DESC;
+
+--DATABASE ADVANCED LESSON
+--JOINS
+
+--creates new table classrooms
+CREATE TABLE classrooms (id INTEGER PRIMARY KEY, student_id integer, section character VARYING(255) not NULL);
+--inserts 10  records in classrooms table
+INSERT into classrooms (id, student_id, section)
+VALUES (1, 1, 'A');
+INSERT into classrooms (id, student_id, section)
+VALUES (2, 2, 'A');
+INSERT into classrooms (id, student_id, section)
+VALUES (3, 3, 'B');
+INSERT into classrooms (id, student_id, section)
+VALUES (4, 4, 'C');
+INSERT into classrooms (id, student_id, section)
+VALUES (5, 5, 'B');
+INSERT into classrooms (id, student_id, section)
+VALUES (6, 6, 'A');
+INSERT into classrooms (id, student_id, section)
+VALUES (7, 7, 'C');
+INSERT into classrooms (id, student_id, section)
+VALUES (8, 8, 'B');
+INSERT into classrooms (id, student_id, section)
+VALUES (9, 9, 'B');
+INSERT into classrooms (id, student_id, section)
+VALUES (10, 10, 'C');
+-- INNER JOIN
+SELECT students.first_name, students.last_name, classrooms.section FROM students INNER JOIN classrooms ON students.id = classrooms.student_id;
+--LEFT JOIN
+SELECT students.id, students.first_name, students.middle_name, students.last_name,students.age,students.location, classrooms.section FROM students LEFT JOIN classrooms ON students.id = classrooms.student_id;
+--RIGHT JOIN
+SELECT students.first_name, students.last_name,classrooms.id, classrooms.student_id, classrooms.section FROM students RIGHT JOIN classrooms ON students.id = classrooms.student_id;
+--FULL JOIN
+SELECT * FROM students FULL JOIN classrooms ON students.id = classrooms.student_id;
